309de959df6610dfb30197b8435f0c7daf8d914d
jram828 <128335805+jram828@users.noreply.github.com>
2024-07-08 21:18:58 -0500

    modificacion controller recordatorio contraseña

--- CRM-AVEZA-main/Server/controllers/usuario/password.js
+++ CRM-AVEZA-main/Server/controllers/usuario/password.js
@@ -1,29 +1,29 @@
 import { models } from "../../DB.js";
 import { sendEmailPassword } from "../../utils/emailNotifier.js";
 
-const { Cliente, Abogado } = models;
+const { Cliente, Abogado, Usuario } = models;
 
 const getPassword = async (email) => {
   console.log('Email get password: ',email)
-  const user = await Abogado.findOne({
+  const user = await Usuario.findOne({
     where: {
       email: email,
     },
   });
-  console.log("Password abogado: ", user.password);
-  if (!user) {
-    const user = await Cliente.findOne({
-      where: {
-        email: email,
-      },
-    });
-    console.log("Password cliente: ", user.password);
+  console.log("Password usuario: ", user.password);
+  // if (!user) {
+  //   const user = await Cliente.findOne({
+  //     where: {
+  //       email: email,
+  //     },
+  //   });
+  //   console.log("Password cliente: ", user.password);
     if (!user) throw new Error("Usuario no encontrado");
     sendEmailPassword(user.nombre, user.email,user.password);
     return user
-  }
-  sendEmailPassword(user.nombre, user.email, user.password);
-  return user;
+  // }
+  // sendEmailPassword(user.nombre, user.email, user.password);
+  // return user;
 };
 
 export { getPassword };
